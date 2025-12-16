3f5a053aa3addf207fb4c665515b3c36825f7b79
ajitdevv <ajitdev014@gmail.com>
2025-12-16 18:29:38 +0530

    Home

---
+++ src/pages/Notfound.jsx
@@ -0,0 +1,21 @@
+import NotfoundImage from "./404page.jpg";
+import { Link } from "react-router-dom";
+export default function NotFoundPage() {
+  return (
+    <>
+      <div>
+        <div className="w-full flex justify-center items-center relative">
+          <img
+            className="w-full h-full flex justify-center items-center "
+            src={NotfoundImage}
+            alt="404 - Page Not Found"
+          />
+
+          <Link to="/" className="cosmic-button absolute bottom-10 right-10">
+            Go Back Home
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+}
